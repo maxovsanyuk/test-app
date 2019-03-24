deb4ecb164f3aff6fe06bd0c5d0b2e15f43e0d4c
Admin <admin@MacBook-Pro-Admin-2.local>
2019-03-24 19:00:17 +0200

    Updates

---
+++ precache-manifest.3717ab3b4557da74ae945b6d6f333042.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf1ae395e12cfec5efa571b22831ee1",
+    "url": "/test-app/static/media/oboitut.com_1528317218.cdf1ae39.png"
+  },
+  {
+    "revision": "557336d4deab6dcb67e3bd72f34fa92e",
+    "url": "/test-app/static/media/loading.557336d4.gif"
+  },
+  {
+    "revision": "7318f3809038590af7ec",
+    "url": "/test-app/static/js/runtime~main.e9adfd5a.js"
+  },
+  {
+    "revision": "51d4996c2f002b7f4818",
+    "url": "/test-app/static/js/main.4617c926.chunk.js"
+  },
+  {
+    "revision": "40c543f83cd30426c6c0",
+    "url": "/test-app/static/js/2.1b13f9de.chunk.js"
+  },
+  {
+    "revision": "51d4996c2f002b7f4818",
+    "url": "/test-app/static/css/main.78c90e3f.chunk.css"
+  },
+  {
+    "revision": "40c543f83cd30426c6c0",
+    "url": "/test-app/static/css/2.3f9e7aef.chunk.css"
+  },
+  {
+    "revision": "47cd8f2ba7e6474030fd0ef85d82a2eb",
+    "url": "/test-app/index.html"
+  }
+];
